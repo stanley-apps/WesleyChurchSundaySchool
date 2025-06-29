8dbec01a915950d1b1ee73a021f3037351e91c13
stanley-apps <tstanleystephen@gmail.com>
2025-06-29 17:13:22 +0530

    Fix TypeScript errors by removing unused React imports

--- src/pages/Songs.tsx
+++ src/pages/Songs.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { supabase, Song } from '../lib/supabase'
 import { ChildFriendlyBackground } from '../components/ChildFriendlyBackground'
