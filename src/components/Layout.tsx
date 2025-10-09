80c6f4250b7d59bdd4e59d4b437c5140a00613d7
[dyad] <tstanleystephen@gmail.com>
2025-10-09 18:41:57 +0530

    [dyad] wrote 1 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, Outlet, useNavigate } from 'react-router-dom'
+import { Link, useLocation, Outlet, useNavigate } from 'react'
 import { useState } from 'react'
 import { useAuth } from '../contexts/AuthContext'
 // import { ChildFriendlyBackground } from './ChildFriendlyBackground' // Removed import
@@ -43,7 +43,7 @@ export function Layout() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen"> {/* Removed bg-gray-50 to allow ChildFriendlyBackground to show */}
       {/* Mobile header */}
       <div className="lg:hidden bg-white shadow-sm border-b">
         <div className="px-4 py-3 flex items-center justify-between">
@@ -160,12 +160,10 @@ export function Layout() {
               </div>
             </div>
           </div>
-        </div>
 
         {/* Main content */}
         <div className="lg:pl-64 flex flex-col flex-1">
           <main className="flex-1">
-            {/* ChildFriendlyBackground removed from here */}
             <Outlet />
           </main>
         </div>
